54766e89ed79473e5f8a8234748cbb38576bfb40
Maximiliano Vargas <maxx7689@gmail.com>
2019-03-25 19:42:53 -0300

    Add files via upload

--- tkinterProyecto.py
+++ tkinterProyecto.py
@@ -2,4 +2,21 @@ from tkinter import *
 
 raiz=Tk()
 
+raiz.title("PREVISION JUBILACION")
+raiz.resizable(1,1)
+#raiz.iconbitmap("afp.icon")
+raiz.geometry("900x720")
+raiz.config(bg="red")
+
+def datos():
+	x=550000
+	result.config(text="Su pension estimada sera de "+str(x))
+
+raiz.config(bg="white")
+miframe=Frame()
+miframe.pack()
+miframe.pack(fill="both",expand="True")
+miframe.config(bg="#56d6df")
+
+
 raiz.mainloop()
